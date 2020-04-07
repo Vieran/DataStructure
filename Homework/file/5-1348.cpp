3726f547577c5f2987a29c61e893a75753b13606
Anonymity <Vieran@sjtu.edu.cn>
2020-04-07 19:03:07 +0800

    using .gitignore

--- Homework/file/5-1348.cpp
+++
@@ -1,3 +0,0 @@
-//2020.4.1---舞会排队
-//链接https://acm.sjtu.edu.cn/OnlineJudge/problem/1348
-//
--- Homework/file/5-4252.cpp
+++
@@ -1,9 +0,0 @@
-//2020.4.1---函数值
-//链接https://acm.sjtu.edu.cn/OnlineJudge/problem/4252
-//多个函数的多个较小值
-
-int main()
-{
-    
-    return 0;
-}
